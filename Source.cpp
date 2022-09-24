dc9d96ad1c8160c90632b38ffc45850f93d41d87
SupineCup <114007597+SupineCup@users.noreply.github.com>
2022-09-24 19:30:19 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	//Boolean1◦ .Дано целое число A.Проверить истинность 
+    //высказывания : «Число A является положительным»
+	//int a;
+	//bool ras;
+	//cin >> a;
+	//res = a > 0;
+	//cout << boolapha << res << endl;
+
+	////Boolean2◦.Дано целое число A.Проверить истинность 
+ //   //высказывания : «Число A является нечетным».
+	///*int a;
+	////bool ras;
+	////cin >> a;
+	////res = a % 2 !== 0;
+	////cout << boolapha << res << endl;
+
+	//Boolean3◦.Дано целое число A.Проверить истинность 
+    //высказывания : «Число A является четным».
+	//int a;
+	//bool res;
+	//cin >> a;
+	//res = a % 2 == 0;
+	//cout << boolapha << res << endl;
+
+	//Boolean4◦.Даны два целых числа : A, B.Проверить истинность 
+    //высказывания : «Справедливы неравенства A > 2 и B ≤ 3»
+	//int a, b;
+	//bool res;
+	//cin >> a >> b;
+	//res = a > 2 && b < 3;
+	//cout << boolapha << res << endl;
+
+}
